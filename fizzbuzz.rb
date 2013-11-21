5522d409982e4430f2bc856cf272f866c8e1d4ea
Giacomo Patella <giacomopatella@gmail.com>
2013-11-21 19:57:09 +0000

    Refactored fizzbuzz again, V2

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,12 @@
-def divisible_by_3?(num)
-  num % 3 == 0
-end
 
-
-def divisible_by_5?(num)
-  num % 5 == 0
-end
-
-
-def divisible_by_15?(num)
-  num % 15 == 0
+def divisible_by?(divisor, num)
+  num % divisor == 0
 end
 
 
 def fizzbuzz(num)
-  return "fizzbuzz" if divisible_by_15?(num)
-  return "fizz" if divisible_by_3?(num)
-  return "buzz" if divisible_by_5?(num)
+  return "fizzbuzz" if divisible_by?(15, num)
+  return "fizz" if divisible_by?(3, num)
+  return "buzz" if divisible_by?(5, num)
   num
 end
